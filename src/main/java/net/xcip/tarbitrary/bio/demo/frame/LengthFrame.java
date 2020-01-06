fca54a0a511278f7d76c22fc11c93dbad39e8c00
tarbitrary <tarbitrary@gmail.com>
2020-01-06 23:49:19 +0800

    长度成帧器完善

--- src/main/java/net/xcip/tarbitrary/bio/demo/frame/LengthFrame.java
+++ src/main/java/net/xcip/tarbitrary/bio/demo/frame/LengthFrame.java
@@ -1,11 +1,13 @@
 package net.xcip.tarbitrary.bio.demo.frame;
 
+import java.io.BufferedOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 public class LengthFrame implements Frame {
     @Override
     public void write2Frame(byte[] buf, OutputStream os) {
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(os);
 
     }
 
